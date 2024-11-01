55ecf2b90599cf6b3b908fe4b4568e0feb941496
João Victor Santana Ferreira <joaovictorsantanaf@gmail.com>
2024-11-01 19:31:42 -0300

    first commit

---
+++ src/main/java/com/topicos/idosos/IdososApplication.java
@@ -0,0 +1,13 @@
+package com.topicos.idosos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdososApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdososApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/topicos/idosos/IdososApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topicos.idosos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdososApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
